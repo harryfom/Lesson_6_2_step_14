be7bf1780b889d506266d218101b082a029508f9
FomeIV <harryfom@gmail.com>
2016-03-25 13:29:21 +0300

    6.2 Коллекции – Шаг 14

---
+++ src/ru/fomenko_iv/lesson6_2/step14/Main.java
@@ -0,0 +1,23 @@
+package ru.fomenko_iv.lesson6_2.step14;
+
+
+import java.util.Set;
+
+/*
+Реализуйте метод, вычисляющий симметрическую разность двух множеств.
+
+Метод должен возвращать результат в виде нового множества. Изменять переданные в него множества не допускается.
+
+Пример
+
+Симметрическая разность множеств {1, 2, 3} и {0, 1, 2} равна {0, 3}.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("6.2 Коллекции – Шаг 14");
+    }
+
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+        return null;
+    }
+}
